e3f457e911f341a3d807f0b9d1750bf58dd1ff44
bodyPiano <bodyPiano>
2023-12-01 22:22:23 -0300

    Add: slash commands ask_to_pai

--- bot.py
+++ bot.py
@@ -28,6 +28,9 @@ COOLDOWN = config.get("cooldown", 120)
 # Load configurations
 configurations = config.get("configs")
 
+# Load configurations
+local_commands = config.get("commands")
+
 if configurations is not None:
     configs_list = [dict({
         "name": configuration.get("name", ""),
@@ -36,6 +39,16 @@ if configurations is not None:
         "image_name": configuration.get("image_name", ""),
         "custom_message": configuration.get("custom_message", "")
     }) for configuration in configurations]
+
+if local_commands is not None:
+    commands_list = [dict({
+        "name": command.get("name", ""),
+        "description": command.get("description", ""),
+        "image_name": command.get("image_name", ""),
+        "custom_message": command.get("custom_message", ""),
+        "links": command.get("links", [])
+    }) for command in local_commands]
+
 
 def on_cooldown(id):
     if id == adm_id:
@@ -47,12 +60,14 @@ def on_cooldown(id):
     else:
         return True
 
+
 def flood_msg_check():
     choice = np.random.randint(1, 11)
     if choice == 1:
         return True
     else:
         return False
+
 
 # INTENTS
 intents = discord.Intents.default()
@@ -66,15 +81,18 @@ TOKEN = os.environ["TOKEN"]
 # Create a bot instance
 bot = commands.Bot(command_prefix='!', intents=intents)
 
+
 # ready log
 @bot.event
 async def on_ready():
     print(f'Logged in as {bot.user.name}')
 
+
 # Event handler for when a message is received
 @bot.event
 async def on_message(message):
-    print(adm_id == message.author.id) # debug
+    print(adm_id == message.author.id)  # debug
+
     # Ignore messages from the bot itself to prevent a loop
     if message.author == bot.user:
         return
@@ -106,9 +124,9 @@ async def on_message(message):
 
             if match_word and (not on_cooldown(message.author.id)):
                 with open(os.environ['IMG_PATH'] + config_instance["image_name"], 'rb') as image_file:
-                        image = discord.File(image_file)
-                        await message.reply(config_instance["custom_message"], file=image)
-                        return
+                    image = discord.File(image_file)
+                    await message.reply(config_instance["custom_message"], file=image)
+                    return
             elif match_word and flood_msg_check():
                 await message.reply('para de floodar seu desgraçado')
 
@@ -126,12 +144,62 @@ async def on_message(message):
 
     await bot.process_commands(message)
 
+
 @bot.slash_command(
     name="paidocs",
     description="documentação do pai"
 )
 async def paidocs(ctx):
     await ctx.respond("PAI BOT\n\n*Comandos*\n1- paibot docs: Documentação Oficial do papai")
+
+
+@bot.slash_command(
+    name="pergunta",
+    description="Links para ensinar as pessoas a perguntarem direito"
+)
+async def ask_to_pai(ctx: discord.ApplicationContext, user: discord.User):
+    ask_data = load_command("pergunta")
+
+    if user is not None and user.id == bot.user.id:
+        await ctx.respond('marcando o bot tá de sacanagem')
+        return
+
+    if user is not None and user.id == ctx.author.id:
+        await ctx.respond('marcando você mesmo tá de sacanagem')
+        return
+
+    if ask_data is not None:
+        if ask_data["image_name"] != "" and ask_data["image_name"] is not None:
+            with open(os.environ['IMG_PATH'] + ask_data["image_name"], 'rb') as image_file:
+                image = discord.File(image_file)
+                await ctx.respond(f'<@{user.id}>' + generate_message(ask_data), file=image)
+        else:
+            await ctx.respond(f'<@{user.id}>' + generate_message(ask_data))
+        print(f'Aks to pai command triggered by {ctx.author.name}')
+    else:
+        await ctx.respond('o admin não configurou o comando direito, pergunta pra ele')
+
+
+def generate_message(command):
+    link_list = command.get("links", [])
+    message = command.get("custom_message", "")
+
+    if link_list:
+        link_string = '\n'.join([f'<{link}>' for link in link_list])
+        message += '\n'.join([f'\n{link_string}'])
+
+    return message
+
+
+def load_command(name):
+    if load_command is not None:
+
+        for command in commands_list:
+            if command["name"] == name:
+                return command
+
+        return None
+
 
 # Start the bot
 bot.run(TOKEN)
